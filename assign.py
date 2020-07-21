1e9bbf41c1cb5032c11b0483fb4d7cabe4317cec
Neha Singh <nehaanitasingh24@gmail.com>
2020-07-21 23:41:07 +0530

    Assignment Day2

---
+++ assign.py
@@ -0,0 +1,87 @@
+print("Hello \
+      python")
+
+"""Use Of \n"""
+print("Hello \npython")
+
+"""To print the value of Variables"""
+name = "ABC"
+marks = 90.248
+age = 19
+print("The name of person is", name, "marks is", marks, "age is", age );  """Method 1 """
+
+print(f"The name of person is {name} the marks is {90.249} age is {age}" );  """Method 1 """
+
+"""Arithmetic Operator"""
+x1 = 5**2
+print(x1)
+
+x2=10+25
+print(x2)
+
+x3=10/5
+print(x3)
+
+x4=10/3
+print(x4)
+
+x5=10%3
+print(x5)
+
+x6=10//3
+print(x6)
+
+x7=10//1
+print(x7)
+
+a = 10
+b = 20
+print(a == b)
+
+c = 10 
+print(a == c)
+print(c <= a)
+print(a>= b)
+print(a != b)
+
+a1 = 240
+b1 = 1
+m= 50
+print(a1 | b1)
+
+bin(a)
+
+bin(b)
+
+print(10 > 9 and 20 > m)
+
+print(10 < 9 and 20 > m)
+
+str1 = "pythonprogramming"
+print("x" in str1)
+
+print("a" in str1)
+
+print("x" not in str1)
+
+x = 1.5
+y = 1.5
+print(x == y)
+
+print(x is y)
+
+id(x)
+
+id(y)
+
+a2 = 257 
+b2 = 257 
+print(a2 == b2)
+
+print(a is b)  # only from -5 to 256 int value are stored in one memory
+
+print(10 + 10 / 29*4)
+
+print(10>8 and 8>9)
+
+
